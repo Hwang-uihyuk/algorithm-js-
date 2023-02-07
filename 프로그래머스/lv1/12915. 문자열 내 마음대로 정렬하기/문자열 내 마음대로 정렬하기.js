c83e362b0e81f074ec0b3844245fdb4d3130e099
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-02-07 12:25:35 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,20 @@
+function solution(strings, n) {
+     strings.sort((a,b) => {
+        if(a[n] < b[n]) return -1 // 음수면 오름차순
+        else if(a[n] > b[n]) return 1
+        else if(a > b) return 1
+        else if(a < b) return -1
+    })
+    return strings;
+}
+
+// function solution(a,b){
+//         a.sort((x, y) => {
+//             if (x[b] < y[b]) {return -1;}
+//             else if (x[b] > y[b]) {return 1;}
+//             else if (x < y) {return -1;}
+//             else if (x > y) {return 1;}
+//             return 0
+//         })
+//         return a
+//     }
