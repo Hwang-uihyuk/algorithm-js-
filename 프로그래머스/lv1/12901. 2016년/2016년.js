3071c53ab3d99a02ff197ced3bea4de60d8c70a3
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-02-17 07:58:09 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+        let arr = ['SUN',"MON","TUE",'WED','THU','FRI','SAT']
+        let qwe = new Date(`'2016/${a}/${b}'`).getDay();
+        
+        return arr[qwe]
+}
