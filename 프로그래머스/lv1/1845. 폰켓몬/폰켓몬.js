c37b708c669691221662f27d2e150a822d498133
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-02-18 16:10:08 +0900

    [level 1] Title: 폰켓몬, Time: 3.48 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,10 @@
 function solution(nums) {
     const getPoket = nums.length/2
+    //이거는 입출력 1일떄 2가 나온다.
     const uniquenums = [...new Set(nums)]
+    
+    
+    
+    console.log([...new Set(nums)])
     return getPoket <= uniquenums.length ? getPoket : uniquenums.length
 }
