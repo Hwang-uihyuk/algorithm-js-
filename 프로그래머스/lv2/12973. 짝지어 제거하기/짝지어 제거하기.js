0c39eb658a85be2bfaac349905f375ee5ccfa0a1
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-09 00:49:25 +0900

    [level 2] Title: 짝지어 제거하기, Time: 37.82 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,18 +1,13 @@
-function solution(s)
-{
-    let answer = 0
-    let stack = []
-    for(let i = 0; i<s.length; i++)
-        {
-            if(stack[stack.length-1] === s.charAt(i))
-                stack.pop()
-                
-            else
-                stack.push(s.charAt(i))
-        }
-    
-      answer = stack.length === 0 ? 1 : 0;
+function solution(s) {
+    const stack = []; // 스택 자료구조를 만듭니다.
 
-    return answer
-    
+    for (const char of s) {
+        if (stack.length > 0 && stack[stack.length-1] === char) {
+            stack.pop(); // 스택에서 짝지어 제거할 문자열을 찾아서 제거합니다.
+        } else {
+            stack.push(char); // 스택에 새로운 문자를 추가합니다.
+        }
+    }
+
+    return stack.length > 0 ? 0 : 1; // 스택에 남은 문자가 없으면 1, 있다면 0을 반환합니다.
 }
