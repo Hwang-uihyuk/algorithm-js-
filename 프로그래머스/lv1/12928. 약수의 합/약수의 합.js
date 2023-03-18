da7ea85c840a216cdd288fec42bacb783fbb4d6d
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-18 09:19:52 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,16 +1,11 @@
 function solution(n) {
-    let res = []
-    if( n === 0)
-        return 0
-    
+    let answer=0
     for(let i = 1; i<=Math.sqrt(n); i++){
-        
-        if( n % i === 0){
-            res.push(i)
-        if( n/i !== i)
-            res.push(n/i)
+        if(n%i === 0 && i !== n/i){
+            answer = answer + i + n/i
         }
+        else if(n%i === 0 && i === n/i) answer += i
+        
     }
-    let ans = res.reduce((a,c)=> a+c)
-    return ans;
+    return answer;
 }
