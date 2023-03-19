5d61496808d79a14c8e6368e5f8697e3f6f34fca
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-19 15:47:08 +0900

    [level 1] Title: K번째수, Time: 6.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,11 +1,16 @@
 function solution(array, commands) {
-    let arr = []
-    for(let i = 0; i<commands.length; i++){
-        let res = array.slice(commands[i][0]-1,commands[i][1])    
-        res.sort((a,b) => a-b)
-        arr.push(res[commands[i][2]-1])
-        }
-        
     
-    return arr;
+    let arr=[]
+    //commands 대로 for문을 돌려주고
+    // 
+    let res = []
+    for(let i=0; i<commands.length; i++){
+       arr = array.slice(commands[i][0]-1, commands[i][1] )
+        arr.sort((a,b)=> a-b)
+            res.push(arr[commands[i][2]-1])
+        console.log(i,arr)
+        console.log(res)
+    }
+    
+    return res;
 }
