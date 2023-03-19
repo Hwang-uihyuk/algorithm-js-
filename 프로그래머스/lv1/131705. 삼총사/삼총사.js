62afb957feb9ead7c5e3d20dca7e5af1eaebd2ee
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-20 08:23:38 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,17 +1,10 @@
 function solution(number) {
-    let cnt = 0 
-    
-    for(let i = 0; i<number.length-2; i++){
+    let cnt = 0
+    for(let i = 0 ; i<number.length-2; i++){
         for(let j = i+1; j<number.length-1; j++){
-            for(let k = j+1; k<number.length; k++){
-                console.log("i", i)
-                console.log("j",j)
-                console.log('k',k)
-                if(number[i]+ number[j]+ number[k] === 0){
-                    
-                    cnt ++
-                }
-                    
+            for(let k = j+1 ; k<number.length; k++){
+                if(number[i] + number[j] + number[k] === 0)   
+                    cnt++
             }
         }
     }
