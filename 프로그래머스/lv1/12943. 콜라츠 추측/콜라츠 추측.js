8003aead171b6ac9942538bed97084e06c3318b4
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-21 09:34:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,18 @@
 function solution(num) {
-    let cnt = 0
-    while( num !== 1){
-        if( num % 2 === 0){
-            num = num /2 
-            cnt ++
-        }
-        else if( num % 2 !==0){
-            num = num*3 + 1
-            cnt ++
-        }
-        if(cnt > 500) return -1
+    var answer = 0;
+    while(num !== 1){
+    if(num % 2 === 0) {
+        num = num /2
+        answer++
+    }
+    else if( num % 2 === 1){
+        num = num * 3 + 1
+        answer ++
+    }
+        
+    if(answer === 500 && num !== 1)
+        return -1
     }
     
-    return cnt 
+    return answer;
 }
