e422cf649ebe76dc31111fbdf75dddc6245cf85c
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-21 10:22:03 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 3.18 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,16 +1,14 @@
 function solution(food) {
-    let arr=[0]
+    var answer = []
     
-    for(let i = food.length-1; i>=1; i--){
-        for(let j = 0; j< parseInt(food[i]/2); j++){
-            arr.push(i)
-            arr.unshift(i)
+    for(let i = 1; i<food.length; i++){
+        for(let j = 0; j<parseInt(food[i]/2); j++){
+            answer.push(i)
         }
         
-        // for(let k = 0; k<parseInt(food[i]/2); k++){
-        //     arr.unshift(i)
-        // }
     }
-    let res = arr.join('')
-    return res;
+    
+    console.log([...answer])
+    
+    return [...answer,0, ...answer.reverse()].join('')
 }
