a90aaeb9f2bab361988435e8c009726401ffba0d
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-22 12:08:00 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,11 @@
 function solution(phone_number) {
-    let num = phone_number
-    num = num.substring(num.length-4,num.length )
-    let a= "*".repeat(num.length-4) + num
-    return "*".repeat(phone_number.length-4) + num
+    let answer = phone_number.slice(0,phone_number.length-4)
+    
+    let lastNum = phone_number.slice(phone_number.length-4, phone_number.length)
+    
+    let val = ''
+    for(let i = 0; i<answer.length; i++){
+        val += '*'
+    }
+    return val + lastNum;
 }
