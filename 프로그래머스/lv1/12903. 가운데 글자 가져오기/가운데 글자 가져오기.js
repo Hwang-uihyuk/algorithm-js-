7f6b23aa4c8cb78702710c72ceaa4b2a4244eece
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-23 10:26:01 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 3.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,10 @@
 function solution(s) {
-    let answer =''
-    answer = s.length % 2 ===0  ? s[parseInt(s.length/2)-1] + s[parseInt(s.length/2)] : s[parseInt(s.length/2)]
+    if(s.length % 2 !== 0)
+        return s[parseInt(s.length/2)]
+    else if(s.length % 2 === 0){
+        console.log(s[s.length/2])
+        return s[ (s.length/2)-1]+s[parseInt(s.length/2)]
+    }
+        
     return answer;
 }
