de6cb9f90013de50b049f52af3bbc7d60b0a3fca
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-27 08:49:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.91 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,7 +1,24 @@
-function solution(s) {
+// function solution(s) {
+//     s = s.toLowerCase().split(' ')
+//     s = s.map(v => v !=='' ? v[0].toUpperCase()+v.slice(1,v.length) : '').join(' ')
+//     // test = 'my. name   is hyu'
+//     // console.log(test.split(' '))
+//     //공백문자를 조심하자.
+    
+//     let test = ["ay", "", "boy","test"]
+//     console.log(test.map(v => v !== "" ? v[0].toUpperCase()+v.slice(1,v.length ): '').join(''))
+//     for(let i =0; i<test.length; i++){
+//         if(test[i] !== '') test[i] = test[i][0].toUpperCase() + test[i].slice(1,test[i].length)
+//         console.log(test[i])
+//         console.log(test.join(' '))
+//     }
+//     return s;
+// }
+
+//공백문자가 연속해서 나올 수 있다.
+function solution(s){
     s = s.toLowerCase().split(' ')
-    s = s.map(v => v !=='' ? v[0].toUpperCase()+v.slice(1,v.length) : '').join(' ')
-    // test = 'my. name   is hyu'
-    // console.log(test.split(' '))
-    return s;
+    console.log(s)
+    s = s.map(v => v ==='' ? '' : v[0].toUpperCase()+v.slice(1,v.length) ).join(' ')
+    return s
 }
