6224053a35ba00d43ff48c8b28943bba5f6c1e48
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-03-29 06:53:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.56 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,29 +1,49 @@
-function solution(board, moves) {
-    var answer = 0;
-    // board [[0,0,0,0,0],
-    //        [0,0,1,0,3],
-    //        [0,2,5,0,1],
-    //        [4,2,4,4,2],
-    //        [3,5,1,3,1]]
+// function solution(board, moves) {
+//     var answer = 0;
+//     // board [[0,0,0,0,0],
+//     //        [0,0,1,0,3],
+//     //        [0,2,5,0,1],
+//     //        [4,2,4,4,2],
+//     //        [3,5,1,3,1]]
     
-    // moves [1,5,3,5,1,2,1,4]	
-    let stack = []
-    let cnt = 0 
-    for(let i = 0 ; i<moves.length; i++){
-        for(let j = 0; j<board.length; j++){
-            if( board[j][moves[i]  - 1] !== 0){
-                let val = board[j][moves[i] - 1] 
-                stack.push(val)
-                board[j][moves[i] -1 ] = 0
-                break;
-            } 
-        }
+//     // moves [1,5,3,5,1,2,1,4]	
+//     let stack = []
+//     let cnt = 0 
+//     for(let i = 0 ; i<moves.length; i++){
+//         for(let j = 0; j<board.length; j++){
+//             if( board[j][moves[i]  - 1] !== 0){
+//                 let val = board[j][moves[i] - 1] 
+//                 stack.push(val)
+//                 board[j][moves[i] -1 ] = 0
+//                 break;
+//             } 
+//         }
         
-         if(stack.length > 1 && stack[stack.length-1] - stack[stack.length-2] ===0  )    {
-             stack.splice(stack.length-2,2)
-             cnt++;
+//          if(stack.length > 1 && stack[stack.length-1] - stack[stack.length-2] ===0  )    {
+//              stack.splice(stack.length-2,2)
+//              cnt++;
              
-         }
+//          }
+//     }
+//     return cnt*2
+// }
+
+function solution(board,moves){
+    let stack = []
+    let cnt = 0
+    for(let i =0; i<moves.length; i++){
+        for(let j = 0; j<board.length; j++){
+            if(board[j][moves[i] - 1] !==0){
+                stack.push(board[j][moves[i] - 1])
+                board[j][moves[i] - 1] = 0
+                break;    
+            } 
+            
+        }
+        if(stack.length > 1 && stack[stack.length -1] - stack[stack.length -2] === 0 ){
+            stack.splice( stack.length -2, 2)
+            cnt ++
+        }
     }
-    return cnt*2
+    return cnt * 2
 }
