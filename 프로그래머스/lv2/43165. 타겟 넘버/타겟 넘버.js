e190fe839b4c4050d7c0836c61ad0c3296f50f63
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-04-01 21:12:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1.06 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,17 +1,39 @@
-function solution(numbers, target) {
-    let cnt = 0;
+// function solution(numbers, target) {
+//     let cnt = 0;
     
-    const dfs = (sum, depth) => {
+//     const dfs = (sum, depth) => {
+        
+//         if(depth === numbers.length){
+//             if(sum === target) cnt ++;
+//             return;
+//         }
+//         dfs(sum + numbers[depth], depth + 1)
+//         dfs(sum - numbers[depth], depth + 1)
+//     }
+    
+//     dfs(0,0);
+    
+//     return cnt;
+// }
+
+function solution(numbers, target){
+    //numbers = [1,1,1,1,1] 
+    //target = 3
+    // 숫자 3을 만들려면 몇가지 방법이 있을까에 대한 
+    let cnt = 0
+    
+    const dfs = (sum,depth) => {
         
         if(depth === numbers.length){
-            if(sum === target) cnt ++;
-            return;
+            if(sum === target) cnt ++
+            return
         }
         dfs(sum + numbers[depth], depth + 1)
-        dfs(sum - numbers[depth], depth + 1)
+        dfs(sum - numbers[depth], depth +1)
+        
+        
     }
-    
-    dfs(0,0);
+    dfs(0,0)
     
     return cnt;
 }
