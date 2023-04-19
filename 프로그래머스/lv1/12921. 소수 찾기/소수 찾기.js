a1491a8f5065d38390e0d5c61b11c9b7d824cffb
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-04-19 16:39:35 +0900

    [level 1] Title: 소수 찾기, Time: 67.73 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -26,16 +26,31 @@
 //     return arr.filter(v => v===true).length;
 // }
 
+// function solution(n){
+//     let arr = new Array(n+1).fill(true).fill(false,0,2)
+    
+//     for(let i = 2; i * i <= n; i++){
+//         if(arr[i] === true){
+//             for(let j = i * i; j <= n; j = j+i){
+//                 arr[j] = false
+//             }
+//         }
+//     }
+//     arr = arr.filter(v => v === true)
+//     return arr.length
+// }
+
 function solution(n){
-    let arr = new Array(n+1).fill(true).fill(false,0,2)
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2)
+    console.log(arr)
     
     for(let i = 2; i * i <= n; i++){
-        if(arr[i] === true){
-            for(let j = i * i; j <= n; j = j+i){
+        for(let j = i * i ; j<= n; j = j+i)
+            if(arr[j]){
                 arr[j] = false
             }
-        }
     }
-    arr = arr.filter(v => v === true)
-    return arr.length
+    return arr.filter(v => v === true).length
+    
+    
 }
