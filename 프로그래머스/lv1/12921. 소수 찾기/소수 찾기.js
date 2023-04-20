47649535f2304940177682711bf2aa348ba2e473
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-04-20 13:54:08 +0900

    [level 1] Title: 소수 찾기, Time: 54.44 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -40,17 +40,34 @@
 //     return arr.length
 // }
 
+// function solution(n){
+//     let arr = Array(n + 1).fill(true).fill(false, 0, 2)
+//     console.log(arr)
+    
+//     for(let i = 2; i * i <= n; i++){
+//         for(let j = i * i ; j<= n; j = j+i)
+//             if(arr[j]){
+//                 arr[j] = false
+//             }
+//     }
+//     return arr.filter(v => v === true).length
+    
+    
+// }
+
+
 function solution(n){
-    let arr = Array(n + 1).fill(true).fill(false, 0, 2)
-    console.log(arr)
+    //1부터 n까지 소수를 구해래ㅏ
+    let arr = new Array(n+1).fill(true).fill(false,0,2)
     
     for(let i = 2; i * i <= n; i++){
-        for(let j = i * i ; j<= n; j = j+i)
-            if(arr[j]){
-                arr[j] = false
-            }
+        
+        for(let j = i * i ; j <=n; j = j + i){
+            if(arr[i]) {
+            arr[j] = false
+        }
+        }
     }
-    return arr.filter(v => v === true).length
-    
-    
+    //true인게 소수인거고 false는 소수가 아닌거임
+    return arr.filter((v) => v === true).length
 }
