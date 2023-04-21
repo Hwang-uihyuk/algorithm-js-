9eca7cb232e34d800e728ded5ccad518aca42c27
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-04-21 16:37:31 +0900

    [unrated] Title: 옹알이 (2), Time: 6.34 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,22 +1,39 @@
-function solution(babbling) {
-    const can = ['aya','ye','woo','ma'];
-    let count = 0;
+// function solution(babbling) {
+//     const can = ['aya','ye','woo','ma'];
+//     let count = 0;
     
-    for(let i = 0; i < babbling.length; i++){
-        let babble = babbling[i];
+//     for(let i = 0; i < babbling.length; i++){
+//         let babble = babbling[i];
         
-        for(let j = 0; j < can.length; j++){
-            if(babble.includes(can[j].repeat(2))){
-                break;
-            }
+//         for(let j = 0; j < can.length; j++){
+//             if(babble.includes(can[j].repeat(2))){
+//                 break;
+//             }
             
-            babble = babble.split(can[j]).join(" ");
-        }
+//             babble = babble.split(can[j]).join(" ");
+//         }
         
+//         if(babble.split(" ").join("").length === 0){
+//             count += 1;
+//         }
+//     }
+    
+//     return count;
+// }
+function solution(babbling){
+    let can = ["aya", "ye", "woo", "ma"] //이것만 발음할 수 있고
+                                         //연속해서 두번발음은 몬함.
+    let count = 0 
+    for(let i = 0; i < babbling.length; i++){
+        let babble = babbling[i] //babbling[0] 는 "aya"
+        for(let j = 0; j<can.length; j++){
+            if(babble.includes(can[j].repeat(2))) //연속 2번나오면 안된다.
+                break;
+            babble = babble.split(can[j]).join(' ')
+            console.log(babble)
+        }
         if(babble.split(" ").join("").length === 0){
             count += 1;
-        }
-    }
-    
-    return count;
+    }}
+    return count
 }
