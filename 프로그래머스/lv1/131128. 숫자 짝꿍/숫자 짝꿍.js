f4c0744adf77b18f46cac8297635b67ac3e7f845
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-04-21 13:55:57 +0900

    [level 1] Title: 숫자 짝꿍, Time: 5.84 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,24 +1,50 @@
-function solution(X, Y) {
-    const answer = []
-    const numberX = new Array(10).fill(0)
-    const numberY = new Array(10).fill(0)
+// function solution(X, Y) {
+//     const answer = []
+//     const numberX = new Array(10).fill(0)
+//     const numberY = new Array(10).fill(0)
     
-    //숫자별 보유 개수파악
-    X.split('').forEach(n => numberX[n] ++)
-    Y.split('').forEach(n => numberY[n] ++)
+//     console.log('X', X.split(''))
+//     //숫자별 보유 개수파악
+//     X.split('').forEach(n => numberX[n] ++)
+//     Y.split('').forEach(n => numberY[n] ++)
+
+//     for(let i = 0; i<10; i++){
+//         const m = Math.min(numberX[i], numberY[i])
+//         for(let j = 0; j < m; j++){
+//             answer.push(i)
+           
+//             console.log('j',j)
+//             console.log(answer)
+//         }
+//     }
+// return answer
+// }
+
+//1회독
+function solution(X,Y){
+    // 0부터 10까지 숫자가 나올 수 있으니까.
+    let numberX = new Array(10).fill(0)
+    let numberY = new Array(10).fill(0)
     
-    //최대공통개수 파악 후, 해당 개수만큼 숫자 확보
-    for(let i = 0; i<10; i++){
+    X.split('').forEach(v => numberX[v]++)
+    Y.split('').forEach(v => numberY[v]++)
+    console.log('x',numberX)
+    
+    let arr = []
+    
+    for(let i = 0; i<numberX.length; i++){
         const m = Math.min(numberX[i], numberY[i])
-        for(let j = 0; j < m; j++){
-            answer.push(i)
+        for(let j = 0; j<m; j++){
+            arr.push(i+"")
         }
+        
     }
-    console.log(numberX, numberY)
-    console.log(answer)
+    arr.sort((a,b)=> b-a)
+    arr = arr.map(v=> v+"").join('')
     
-    let mateNum = answer.sort((a,b) => b-a)
-    if(mateNum[0] == 0){ mateNum = [0]}
-    if(mateNum.length == 0) { mateNum = [-1]}
-    return String(mateNum.join(''));
+    if(arr.length === 0) return "-1"
+    else if(arr[0] === "0") return "0"
+    else return arr
+    
+    
 }
