6d44bc6080193c77a32af3412cc1ab2f8fc2335e
Hwang-uihyuk <79883776+Hwang-uihyuk@users.noreply.github.com>
2023-06-08 18:31:21 +0900

    [unrated] Title: 옹알이 (2), Time: 3.54 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -20,20 +20,38 @@
     
 //     return count;
 // }
+// function solution(babbling){
+//     let can = ["aya", "ye", "woo", "ma"] //이것만 발음할 수 있고
+//                                          //연속해서 두번발음은 몬함.
+//     let count = 0 
+//     for(let i = 0; i < babbling.length; i++){
+//         let babble = babbling[i] //babbling[0] 는 "aya"
+//         for(let j = 0; j<can.length; j++){
+//             if(babble.includes(can[j].repeat(2))) //연속 2번나오면 안된다.
+//                 break;
+//             babble = babble.split(can[j]).join(' ')
+//             console.log(babble)
+//         }
+//         if(babble.split(" ").join("").length === 0){
+//             count += 1;
+//     }}
+//     return count
+// }
+
+
 function solution(babbling){
-    let can = ["aya", "ye", "woo", "ma"] //이것만 발음할 수 있고
-                                         //연속해서 두번발음은 몬함.
-    let count = 0 
-    for(let i = 0; i < babbling.length; i++){
-        let babble = babbling[i] //babbling[0] 는 "aya"
-        for(let j = 0; j<can.length; j++){
-            if(babble.includes(can[j].repeat(2))) //연속 2번나오면 안된다.
+    let cnt = 0;
+    let can = ["aya", "ye", "woo", "ma"]
+    // ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	
+    for(let i =0; i<babbling.length; i++){
+        for(let j =0; j<can.length; j++){
+            if(babbling[i].includes(can[j].repeat(2)))
                 break;
-            babble = babble.split(can[j]).join(' ')
-            console.log(babble)
+            babbling[i] = babbling[i].split(can[j]).join(' ')                        
         }
-        if(babble.split(" ").join("").length === 0){
-            count += 1;
-    }}
-    return count
+        console.log(babbling[i])
+        if(babbling[i].split(' ').join('') === "") cnt++
+        
+    }
+    return cnt
 }
